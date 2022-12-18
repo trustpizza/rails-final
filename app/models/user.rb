bcdfe0a5a730f396e00afc6dcb4236cd8e747657
trustpizza <maxelolsson@gmail.com>
2022-12-17 18:23:50 -0600

    Add friendship routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
 
   has_many :sent_friend_requests, foreign_key: :sender_id, class_name: 'FriendRequest', dependent: :destroy
   has_many :received_friend_requests, foreign_key: :receiver_id, class_name: 'FriendRequest', dependent: :destroy
-  
+
+  # Friends modeling
+
   has_many :began_friendships, foreign_key: :user_id, class_name: "Friendship", dependent: :destroy
   has_many :accepted_friendships, foreign_key: :friend_id, class_name: "Friendship", dependent: :destroy
   has_many :began_friends, through: :began_friendships, source: :friend, dependent: :destroy
   has_many :accepted_friends, through: :accepted_friendships, source: :user, dependent: :destroy
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do 
     resources :friend_requests
+    resources :friendships
   end
   # Defines the root path route ("/")
   # root "articles#index"
