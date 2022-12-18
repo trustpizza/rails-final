2b45f16fe50de0f8d4f4f9551a5ea23dd4736e5f
trustpizza <maxelolsson@gmail.com>
2022-12-17 18:06:24 -0600

    Create fr controller skeleton

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,21 @@
 class FriendRequestsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def friend_request_params
+
+  end
 end
