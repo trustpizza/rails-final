518bf22957119e054240862f77b8927ac7f42753
trustpizza <maxelolsson@gmail.com>
2022-12-17 18:01:13 -0600

    Add modeling between users and friendrequsts

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,2 +1,4 @@
 class FriendRequest < ApplicationRecord
+  belongs_to :sender, class_name: 'User'
+  belogns_to :receiver, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Friend Request Modeling
+  
+  has_many :sent_friend_requests, foreign_key: :sender_id, class_name: 'FriendRequest', dependent: :destroy
+  has_many :received_friend_requests, foreign_key: :receiver_id, class_name: 'FriendRequest', depdent: :destroy
+    
 end
