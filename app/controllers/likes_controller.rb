be8c8d69ba334f032e2dee954e00aa3fb280ffe2
trustpizza <maxelolsson@gmail.com>
2022-12-18 13:03:18 -0600

    Create likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,23 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.find(like_params[:post_id])
+    @like = @post.likes.create(like_params)
+    redirect_to request.referrer
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.find_by(like_params)
+    @like.destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
+  end
 end
