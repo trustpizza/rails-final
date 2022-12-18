92a88b71acdfcf7c19f6b6734ab856fefe5580a3
trustpizza <maxelolsson@gmail.com>
2022-12-18 13:25:54 -0600

    Finish comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post 
   belongs_to :commenter, class_name: "User"
 
-  validates :body, presence: true, length: { minimum: 1 }
+  validates :body, presence: true, length: { minimum: 1 } 
 end
