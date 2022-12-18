11bcad9508afe2a23c4cb5aff7f9edb3fcd8bd70
trustpizza <maxelolsson@gmail.com>
2022-12-18 13:29:29 -0600

    Ran DB;migrate

--- db/migrate/20221218192825_rename_users_to_author_for_comments.rb
+++ db/migrate/20221218192825_rename_users_to_author_for_comments.rb
@@ -1,4 +1,4 @@
-class RenameUsersToCommenterForComments < ActiveRecord::Migration[7.0]
+class RenameUsersToAuthorForComments < ActiveRecord::Migration[7.0]
   def change
     rename_column :comments, :user_id, :commenter_id
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_190350) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_192825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "commenter_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["commenter_id"], name: "index_comments_on_commenter_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -62,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_190350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "commenter_id"
   add_foreign_key "friend_requests", "users", column: "receiver_id"
   add_foreign_key "friend_requests", "users", column: "sender_id"
   add_foreign_key "friendships", "users"
