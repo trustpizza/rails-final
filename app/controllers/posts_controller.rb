c04cad5c2a992ff53928bf584632f1af463434bf
trustpizza <maxelolsson@gmail.com>
2022-12-17 23:52:02 -0600

    Complete post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,48 @@
 class PostsController < ApplicationController
+  class PostsController < ApplicationController
+    before_action :authenticate_user!
+  
+    def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
+  
+  
+    def create
+      @post = current_user.posts.build(post_params)
+  
+      if @post.save
+        redirect_to root_path
+        flash[:notice]='Post was created'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+  
+      redirect_to root_path
+      flash[:alert] = 'Post Deleted'
+    end
+  
+    def edit
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+    end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:body, :title)
+    end
+  end
 end
