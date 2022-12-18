ac8c4c6ae0b774b5604785c6364d8b87ed7e066b
trustpizza <maxelolsson@gmail.com>
2022-12-17 18:04:15 -0600

    Add friend request routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users do 
+    resources :friend_requests
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
