ede5ce9aac89e8484c1a31ba845afb3d1a1979ab
trustpizza <maxelolsson@gmail.com>
2022-12-18 13:47:27 -0600

    Create comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find_by(params[:id])
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to request.referrer
+      flash[:notice] = "Comment posted."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :commenter_id)
+  end
 end
